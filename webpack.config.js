11279ff6ed8228250908a5ea4c7ec58d0dbace01
Javier Samaniego Valero <jsamaniego@cjf.gob.mx>
2021-01-24 12:35:58 -0600

    React RouterDOM

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -51,7 +52,8 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 3000,
-    open: true,
+    historyApiFallback: true,
+    port: 3005,
+    open: false,
   },
 };
