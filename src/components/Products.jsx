6ddc4f1bd3f7a0581db163cc9d56429d7b2e4930
Javier Samaniego Valero <jsamaniego@cjf.gob.mx>
2021-01-25 13:50:06 -0600

    Implementando useContext

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Product from './Product';
+import AppContext from '../context/AppContext';
 import '../styles/components/Products.css';
 
-const Products = ({ products }) => {
+const Products = () => {
+  const { state, addToCart } = useContext(AppContext);
+  const { products } = state;
+
+  //Una funcion que llama a otra fincion, es un closure
+  //La ejecución del closure retorna otra función o callback que esta listo para ser ejecutado. En el caso del ejemplo cuando se ejecuta el método render del componente se ejecuta el closure y retorna otra función y es en el onClick que se ejecuta la función previamente retornada. El closure retorna la función más no la ejecuta.Por otro lado, la ejecución del la función tradicional no retorna nada porque ya se ejecuta de una vez addToCart.
+
+  const handleAddToCart = (product) => () => {
+    addToCart(product);
+  };
+
   return (
     <div className="Products">
       <div className="Products-items">
         {products.map((product) => (
-          <Product key={product.id} product={product} />
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          />
         ))}
       </div>
     </div>
